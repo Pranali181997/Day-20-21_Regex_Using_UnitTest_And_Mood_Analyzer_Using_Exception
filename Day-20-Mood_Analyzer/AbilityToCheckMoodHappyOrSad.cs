218732cd031e8c56d923a60f6ece82f8ef5010ef
Pranali <pranalilambat00@gmail.com>
2022-03-06 11:49:47 +0530

    Full folder

---
+++ Day-20-Mood_Analyzer/AbilityToCheckMoodHappyOrSad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mood_Analyzer
+{
+    public class AbilityToCheckMoodHappyOrSad
+    {
+        // public string message;
+        public string MoodAna(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ Day-20-Mood_Analyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mood_Analyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Mood Analyser Problem!");
+
+
+            AbilityToCheckMoodHappyOrSad moodanalyser = new AbilityToCheckMoodHappyOrSad();
+            string res = moodanalyser.MoodAna("happy");
+            //string res1 = moodanalyser.} HandleException("I am in happy mood");
+                Console.WriteLine(res);
+               // Console.WriteLine(res1);
+        }
+        }
+    }
